fe7ebdb475aae424c0a41c707eafa1552e598797
P-DR0ZD <drozdpiotr@outlook.com>
2022-10-24 17:19:49 -0400

    Build

--- my-website/docusaurus.config.js
+++ my-website/docusaurus.config.js
@@ -6,19 +6,18 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'P-DR0ZD', // Usually your GitHub org/user name.
-  projectName: ' pdrozdSSG-Docusaurus', // Usually your repo name.
-
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
-  url: `https://${this.organizationName}.github.io`,
-  baseUrl: `/${this.projectName}/`,
+  url: 'https://your-docusaurus-test-site.com',
+  baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'facebook', // Usually your GitHub org/user name.
+  projectName: 'docusaurus', // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
@@ -37,13 +36,15 @@ const config = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl: `https://github.com/${this.organizationName}/${this.projectName}/tree/main/`,
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl: `https://github.com/${this.organizationName}/${this.projectName}/tree/main/`,
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
